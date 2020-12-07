82ab47ceaae0356c1e0b681e6dafe375e6dd4912
Ernest Cheung <ernestcheung@trustmechain.com>
2020-12-07 22:48:11 +0800

    [CounterSizeGenerator][Ernest] Feat: added styling

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react';
 export default class CounterSizeGenerator extends Component {
     render() {
         return (
-            <input type="number" value={this.props.size} onChange={this.props.onChange} />
+        <section 
+            style={
+                {
+                 border: '2px solid white',
+                 padding: '15px'
+                }
+            }
+        >
+            <span>
+                size: <input type="number" value={this.props.size} onChange={this.props.onChange} />
+            </span>
+        </section>
         )
     }
 }
